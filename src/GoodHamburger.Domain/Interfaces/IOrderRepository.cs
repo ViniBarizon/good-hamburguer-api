edf3f2d5170462577e4193fb36513475fc9cce43
Vinicius Peres Barizon <vi.barizont@gmail.com>
2026-04-22 02:17:16 -0300

    feat: add repository interfaces

---
+++ src/GoodHamburger.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,8 @@
+public interface IOrderRepository
+{
+    Task<Order?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Order>> GetAllAsync();
+    Task AddAsync(Order order);
+    Task UpdateAsync(Order order);
+    Task DeleteAsync(Guid id);
+}
