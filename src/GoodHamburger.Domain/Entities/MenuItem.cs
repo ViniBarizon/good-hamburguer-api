fc01d9a3f952b17116ce51fc575d282f900bcdef
Vinicius Peres Barizon <vi.barizont@gmail.com>
2026-04-22 02:08:20 -0300

    feat: add MenuItem entity

---
+++ src/GoodHamburger.Domain/Entities/MenuItem.cs
@@ -0,0 +1,12 @@
+public class MenuItem
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+    public ProductType Type { get; private set; }
+
+    public MenuItem(int id, string name, decimal price, ProductType type)
+    {
+        Id = id; Name = name; Price = price; Type = type;
+    }
+}
