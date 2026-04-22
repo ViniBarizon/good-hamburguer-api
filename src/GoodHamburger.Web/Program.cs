5e012040194fc43d87d1e896d27f69a71fe9366f
Vinicius Peres Barizon <vi.barizont@gmail.com>
2026-04-22 04:15:12 -0300

    feat: add Blazor WebAssembly frontend with menu and order management

---
+++ src/GoodHamburger.Web/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using GoodHamburger.Web;
+using GoodHamburger.Web.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<ApiService>();
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("http://localhost:5133/")
+});
+
+await builder.Build().RunAsync();
